466e1de8eb6e0677922e556857af8be86543ca6c
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2023-03-16 15:00:23 +0300

    feat: form autocomplete with suggestion

--- components/hint-widget/hint-widget.js
+++ components/hint-widget/hint-widget.js
@@ -16,128 +16,181 @@ const TYPES = {
   'LEGAL': 'Организация'
 }
 
-class HintWidget extends HTMLElement {
-  constructor() {
-    super();
-    this.setAttribute('suggestions', null);
-    this.current = {
-      shortName: this.getAttribute('shortName') || '',
-      fullName: this.getAttribute('fullName') || '',
-      inn: this.getAttribute('inn') || '',
-      address: this.getAttribute('address') || '',
-    }
-  }
-
-  // get placeholder() {
-  //   return this.getAttribute("placeholder") || "";
-  // }
-
-  // get suggestions() {
-  //   return this.hasAttribute("suggestions") && this.getAttribute("suggestions") !== "false";
-  // }
-
-  // set open(value) {
-  //   value === true
-  //     ? this.setAttribute("open", "")
-  //     : this.removeAttribute("open");
-  // }
-  
-  
-  static get observedAttributes() {
-    return ['query', 'suggestions'];
-  }
-
-  fetchSuggestions = async (e) => {
-      fetch(url, {
-        ...options,
-        body: JSON.stringify({
-          query: e.target.value,
-          count: 5,
-          type: "PARTY",
-        }),
-        }
-      )
-      .then(response => response.text())
-      .then(result => {
-        const data = JSON.parse(result).suggestions;
-        this.setAttribute('suggestions', JSON.stringify([...data]));
-      })
-      .catch(error => console.log("error", error));
-  }
-
-  connectedCallback () {
-    this.render();
-    setTimeout(() => {
-      document.querySelector('#party').addEventListener('change', this.fetchSuggestions);
-    });
-  }
-
-  
-  render() {
-    console.log('render');
-    const { shortName, fullName, inn, address} = this.current;
-    const data = JSON.parse(this.getAttribute('suggestions'));
-    console.log(data);
-    data?.forEach(item => {
-      const li = document.createElement('li');
-      item.innerHTML = item.value;
-      this.querySelector('.suggestions')?.appendChild(li);
-    })
-
-    this.innerHTML = `
-    <style>
-      @import "/components/hint-widget/hint-widget.css";
-    </style>
-    <section class="container">
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    @import "/components/hint-widget/hint-widget.css";
+  </style>
+  <div class="hint-widget">
+    <section class="search">
       <p><strong>Компания или ИП</strong></p>
       <input id="party" name="party" type="text" placeholder="Введите название, ИНН, ОГРН или адрес организации" />
-      ${this.suggestions && `<ul class="suggestions">${suggestionElems}<ul>`}
+      <ul class="suggestions">
     </section>
 
     <section class="result">
       <p id="type"></p>
       <div class="row">
         <label>Краткое наименование</label>
-        <input id="name_short" value=${shortName}>
+        <input id="name_short">
       </div>
       <div class="row">
         <label>Полное наименование</label>
-        <input id="name_full" value=${fullName}>
+        <input id="name_full">
       </div>
       <div class="row">
         <label>ИНН / КПП</label>
-        <input id="inn_kpp" value=${inn}>
+        <input id="inn_kpp">
       </div>
       <div class="row">
         <label>Адрес</label>
-          <input id="address value=${address}">
+          <input id="address">
       </div>
     </section>
-    `;
+  </div>
+`;
+
+class HintWidget extends HTMLElement {
+  constructor() {
+    super();
+    // this.setAttribute('suggestions', null);
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this._suggestions = [];
+    this.isOverList = false;
+
+    this.searchEl = this.shadowRoot.querySelector('#party');
+    this.suggestionsEl = this.shadowRoot.querySelector('.suggestions');
+    this.typeEl = this.shadowRoot.querySelector('#type');
+    this.nameShortEl = this.shadowRoot.querySelector('#name_short');
+    this.nameFullEl = this.shadowRoot.querySelector('#name_full');
+    this.innEl = this.shadowRoot.querySelector('#inn_kpp');
+    this.addressEl = this.shadowRoot.querySelector('#address');
+    }
+
+  set suggestions(value) {
+    this._suggestions = [...value];
   }
-  // Whenever an attibute is changed, this function is called. A switch statement is a good way to handle the various attributes.
-  // Note that this also gets called the first time the attribute is set, so we do not need any special initialisation code.
-  attributeChangedCallback() {
-    this.render();
+  get suggestions() {
+    return this._suggestions;
   }
-  // attributeChangedCallback(name, oldValue, newValue) {
-  //   switch(name) {
-  //     case 'author':
-  //       this.querySelector('.author').innerText = newValue;
-  //       this.querySelector('.message').classList.toggle('self', newValue === 'Me');
-  //       break;
-  //     case 'profile-photo':
-  //       this.querySelector('.profile-photo').setAttribute('src', newValue);
-  //       break;
-  //     case 'message-text':
-  //       this.querySelector('.message-text').innerText = newValue;
-  //       break;
-  //     case 'time':
-  //       this.querySelector('time').innerText = newValue;
-  //       break;
-  //   }
-  // }
+
+  static get observedAttributes() {
+    return ['open', 'query'];
+  }
+
+  fetchSuggestions = async (query) => {
+      fetch(url, {
+        ...options,
+        body: JSON.stringify({
+          query: query,
+          count: 5,
+          // type: "PARTY",
+        }),
+        }
+      )
+      .then(response => response.text())
+      .then(result => {
+        const data = JSON.parse(result).suggestions;
+        this.suggestions = [...data];
+        this.setAttribute('open', true);
+        this.setAttribute('query', query);
+
+      })
+      .catch(error => console.log("error", error));
+  }
+
+  async handleInput  (e) {
+    const query = e.target.value;
+    if (query) { 
+      this.fetchSuggestions(query);
+    } else {
+      this.setAttribute('open', false);
+      this.setAttribute('query', '');
+    }
+  }
+
+  connectedCallback () {
+    setTimeout(() => {
+      this.searchEl.addEventListener('input', this.handleInput.bind(this));
+      this.searchEl.addEventListener('blur', () => {
+        if (!this.isOverList) {this.setAttribute('open', false)}
+      });
+      this.searchEl.addEventListener('focus', () => {
+        if (this.getAttribute('query')) this.setAttribute('open', true)
+      });
+    });
+  }
+
+  renderSuggestions() {
+    this.suggestionsEl.innerHTML = '';
+    if (this.suggestions.length > 0) {
+      const hint = document.createElement('p');
+      hint.className = 'suggestions__hint';
+      hint.innerHTML = 'Выберите вариант или продолжите ввод';
+      this.suggestionsEl.appendChild(hint);
+
+      this.suggestions.forEach(item => {
+        const li = document.createElement('li');
+        li.className = "suggestions__item";
+        const title = document.createElement('p');
+        title.className = 'title';
+        title.innerHTML = item.value;
+        const info = document.createElement('p');
+        info.className = 'info';
+        info.innerHTML = item.data.inn + ' ' + item.data.address.value;
+        
+        li.appendChild(title);
+        li.appendChild(info);
+
+        li.addEventListener('click', () => this.handleSelect(item.data));
+        this.suggestionsEl.appendChild(li);
+        this.suggestionsEl.addEventListener('mouseover', () => this.isOverList = true);
+        this.suggestionsEl.addEventListener('mouseleave', () => this.isOverList = false)
+      });
+    }
+  }
+
+  toggleOpen () {
+    this.suggestionsEl.style.display = this.getAttribute('open') ==='true' ? "block" : 'none';
+  }
+
+  handleSelect(data) {
+    if (!data) return;
+
+    this.typeEl.innerHTML = TYPES[data.type] + " (" + data.type + ")"
+
+    this.searchEl.value = data.name.short_with_opf;
+    if (data.name) {
+      this.nameShortEl.value = data.name.short_with_opf || "";
+      this.nameFullEl.value = data.name.full_with_opf || "";
+    }
   
+    this.innEl.value = join([data.inn, data.kpp], " / ");
+
+    if (data.address) {
+      var address = "";
+      if (data.address.data.qc == "0") {
+        address = join([data.address.data.postal_code, data.address.value]);
+      } else {
+        address = data.address.data.source;
+      }
+      this.addressEl.value = address;
+    }
+    this.setAttribute('query', '');
+    this.setAttribute('open', false);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch(name) {
+      case 'open':
+        this.toggleOpen();
+        break;
+      case 'query':
+        this.renderSuggestions();
+        break;
+    }
+  }
 
 }
 
@@ -149,39 +202,3 @@ function join(arr /*, separator */) {
   var separator = arguments.length > 1 ? arguments[1] : ", ";
   return arr.filter(function(n){return n}).join(separator);
 }
-
-function showSuggestion(suggestion) {
-  console.log(suggestion);
-  var data = suggestion.data;
-  if (!data)
-    return;
-  
-  // $("#type").value =
-  //   TYPES[data.type] + " (" + data.type + ")"
-
-
-  // if (data.name) {
-  //   $("#name_short").val(data.name.short_with_opf || "");
-  //   $("#name_full").val(data.name.full_with_opf || "");
-  // }
-    
-  // $("#inn_kpp").val(join([data.inn, data.kpp], " / "));
-  
-  // if (data.address) {
-  //   var address = "";
-  //   if (data.address.data.qc == "0") {
-  //     address = join([data.address.data.postal_code, data.address.value]);
-  //   } else {
-  //     address = data.address.data.source;
-  //   }
-  //   $("#address").val(address);
-  // }
-}
-
-// $("#party").suggestions({
-//   token: token,
-//   type: "PARTY",
-//   count: 5,
-//   /* Вызывается, когда пользователь выбирает одну из подсказок */
-//   onSelect: showSuggestion
-// });
